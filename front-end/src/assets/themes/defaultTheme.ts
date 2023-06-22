b33e93bfd169d057a3d10c92324856507af33376
Lucas dos Santos <lucas.ffr2014@gmail.com>
2023-06-22 11:37:29 -0300

    🌈 style(Font-Family):
    Adicionado font family roboto

--- front-end/src/assets/themes/defaultTheme.ts
+++ front-end/src/assets/themes/defaultTheme.ts
@@ -1,6 +1,11 @@
 import { extendTheme, ToastProviderProps } from '@chakra-ui/react'
 
-export const defaultTheme = extendTheme({})
+export const defaultTheme = extendTheme({
+  fonts: {
+    body: `'Roboto', sans-serif`,
+    heading: `'Roboto', sans-serif`,
+  },
+})
 
 export const toastOptions: ToastProviderProps = {
   defaultOptions: { position: 'top-right', isClosable: true, duration: 3000 },
